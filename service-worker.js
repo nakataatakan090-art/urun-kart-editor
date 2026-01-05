4885b358aadc5bc6376b7f7924651b8856e15f6e
nakataatakan090-art <nakata.atakan.090@gmail.com>
2026-01-05 08:08:05 +0300

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE = "urun-kart-offline-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
